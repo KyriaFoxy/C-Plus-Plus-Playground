373011ebed7bde9b526c1a976e9da2d0bae4b4d3
Astryda Malinowska <astamalinowska@outlook.com>
2022-12-06 19:34:17 +0100

    Initial Commit

---
+++ Playground/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Playground
+//
+//  Created by Refr Asta on 06/12/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
