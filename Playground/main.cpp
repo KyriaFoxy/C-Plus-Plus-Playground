c780553a32dcc37d6537a2e0ee597931cc48a1e0
Astryda Malinowska <astamalinowska@outlook.com>
2022-12-06 20:08:03 +0100

    first conditional and first function

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -6,13 +6,60 @@
 //
 
 #include <cstdio>
+int step_function(int);
 
 int main(int argc, const char * argv[])
 {
-    //1.1
+    //1.1 Simple hello world
     printf("Hello world of C++!\n");
     puts("This is my playground");
     
+    //1.2 Comparision operators - no value printed
+    int x = 0;
+    42 != x;
+    100 > x;
+    123 >= x;
+    -10 < x;
+    -99 <= x;
+    
+    //1.4 Conditional
+    int num = 0;
+    if (num > 0)
+    {
+        puts("Positive.");
+    }
+    else if (num < 0)
+    {
+        puts("Negative.");
+    }
+    else
+    {
+        puts("Zero.");
+    }
+    
+    //1.6 little step function
+    puts("Let's check if number is positive or negative");
+    int value1 = step_function(6);
+    printf("%d\n", value1);
+    int value2 = step_function(-100);
+    printf("%d\n", value2);
+    int value3 = step_function(0);
+    printf("%d\n", value3);
+    
     
     return 0;
+}
+
+int step_function(int num)
+{
+    int result = 0;
+    if (num < 0)
+    {
+        result = -1;
+    }
+    else if (num > 0)
+    {
+        result = 1;
+    }
+    return result;
 }
