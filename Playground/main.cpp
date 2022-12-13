758dffa82c9a1c897cfdcef21bab5a8496efd40a
Astryda Malinowska <astamalinowska@outlook.com>
2022-12-13 15:50:33 +0100

    First pointer

--- Playground/main.cpp
+++ Playground/main.cpp
@@ -11,5 +11,17 @@ int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
     puts("Reference types from chapter 3");
+    
+    //3.1 address pointing
+    int gettysburg{};
+    printf("gettysburg: %d.\n", gettysburg);
+    int *gettysburg_address = &gettysburg;
+    printf("&gettysburg: %p.\n", gettysburg_address);
+    /*
+     gettysburg: 0.
+     &gettysburg: 0x16fdff20c.
+     */
+    
+    
     return 0;
 }
